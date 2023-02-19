930b25298e2ee5cdcf46bd24def088c0918af046
Stanislav Ponomarev <ponomarev.ca@gmail.com>
2023-02-19 18:55:45 +0100

    Vim basic configuration

--- vim/setup.sh
+++ vim/setup.sh
@@ -3,7 +3,7 @@
 set -x
 
 cp -f .vimrc ~/
-cp -f .vimrc.plug ~/.vimrc.plug
+cp -f .vimrc.plug ~/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
